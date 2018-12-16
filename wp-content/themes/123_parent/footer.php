6b7504fd19d02685cf5a9d6ebe18a692739c3f70
kyle <kyle.marcy@gmail.com>
2018-12-16 09:23:22 -0500

    branching into some general refactoring

--- wp-content/themes/123_parent/footer.php
+++ wp-content/themes/123_parent/footer.php
@@ -1,14 +1,28 @@
 <?php
-
+/**
+ * 
+ */
 	if( $post->post_name !== 'disabled' ){
+
 		get_template_part('partials/global', 'popups');
+
+		// get/set whatever the footers need:
+		
+
+
+		
+		// include mobile/desktop footers
 		get_template_part('partials/navigation/footer', 'mobile');
 		get_template_part('partials/navigation/footer', 'desktop');
+
 	}
 
 	if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
-	   ?> <script src="//localhost:35729/livereload.js"></script> <?php
+	   ?>
+	   	<script src="//localhost:35729/livereload.js"></script>
+	   <?php
 	}
+
 	wp_footer();
 ?>
 </body>
