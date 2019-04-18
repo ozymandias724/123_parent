bf35a4d5a92c6f07e988feba065642ae7e06ea01
Gus <gusta1296@gmail.com>
2019-04-18 09:42:28 -0700

    Added masonry grid for Standard gallery without tabs.

--- wp-content/themes/123_parent/modules/mod.gallery.php
+++ wp-content/themes/123_parent/modules/mod.gallery.php
@@ -21,22 +21,21 @@
 
     // Type of tabs
     $fields['tabs_type'] = 'tabs_side_menu';
-    $fields['masonry'] = true; 
+    $fields['masonry'] = false; 
  
     // check type
     // Standard
     if( $fields['type'] == 'standard' ){  
         
         if($fields['masonry'] == true){
-            $format_gallery = '<li><img class="grid_image" src="%s" style="height:%spx;" /></li>';
+            $format_gallery = '<li><img class="grid_image" src="%s" /></li>';
             
             $return_gallery = '<div id="standard_gallery" class="grid_gallery"><h3>' . $fields['normal_gallery']['heading'] . '</h3><ul class="grid_masonry">';
             
             foreach( $fields['normal_gallery']['images'] as $i => $image ){
                 $return_gallery .= sprintf(
                     $format_gallery,
-                    $image['url'],
-                    $image['sizes']['medium-height']
+                    $image['sizes']['medium_large']
                 );
             }
     
@@ -61,42 +60,46 @@
     // Tabbed Gallery
     } else if($fields['type'] == 'tabbed'){
 
-        $format_gallery = '<h3><a class="%s %s" href="javascript:;">%s</a></h3>';
+        if($fields['masonry'] == true){
+            
+        }else{
+            $format_gallery = '<h3><a class="%s %s" href="javascript:;">%s</a></h3>';
 
-        $return_gallery = '<div id="tabbed_gallery" class="' . $fields['tabs_type'] . '"><div>';
-        
-        foreach($fields['tabbed_gallery'] as $i => $tab){
-            $return_gallery .= sprintf(
-                $format_gallery ,
-                strtolower(str_replace(' ', '', $tab['tab_title'])) . "_tab",
-                ($i === 0 ? "active_gallery" : ""),
-                $tab['tab_title']
-            );
-        }
-
-        $return_gallery .= '</div>'; 
-
-        $gallery_list = '<div>'; 
- 
-        foreach($fields['tabbed_gallery'] as $i => $tab){
-
-            $tab_title = strtolower(str_replace(' ', '', $tab['tab_title'])). '_section ';
-            $active_gallery_section = ($i === 0 ? "active_gallery_section" : "");
-
-            $gallery = '<ul class="gallery_section ' . $tab_title . $active_gallery_section . '">';  
-            foreach($tab['images'] as $image){
-                $image_url = $image['url'];
-                $gallery .= '<li><div class="image" style="background-image:url(' . $image_url . ');"></div></li>';
+            $return_gallery = '<div id="tabbed_gallery" class="non_grid ' . $fields['tabs_type'] . '"><div>';
+            
+            foreach($fields['tabbed_gallery'] as $i => $tab){
+                $return_gallery .= sprintf(
+                    $format_gallery ,
+                    strtolower(str_replace(' ', '', $tab['tab_title'])) . "_tab",
+                    ($i === 0 ? "active_gallery" : ""),
+                    $tab['tab_title']
+                );
             }
-
-            $gallery .= '</ul>';
-            $gallery_list .=  $gallery;
+    
+            $return_gallery .= '</div>'; 
+    
+            $gallery_list = '<div>'; 
+     
+            foreach($fields['tabbed_gallery'] as $i => $tab){
+    
+                $tab_title = strtolower(str_replace(' ', '', $tab['tab_title'])). '_section ';
+                $active_gallery_section = ($i === 0 ? "active_gallery_section" : "");
+    
+                $gallery = '<ul class="gallery_section ' . $tab_title . $active_gallery_section . '">';  
+                foreach($tab['images'] as $image){
+                    $image_url = $image['url'];
+                    $gallery .= '<li><div class="image" style="background-image:url(' . $image_url . ');"></div></li>';
+                }
+    
+                $gallery .= '</ul>';
+                $gallery_list .=  $gallery;
+            }
+            
+    
+            $return_gallery .= $gallery_list . '</div>';
+    
+            $return_gallery .= '</div>';
         }
-        
-
-        $return_gallery .= $gallery_list . '</div>';
-
-        $return_gallery .= '</div>';
     }   
  
 ?>
@@ -104,5 +107,6 @@
 <?php 
     echo get_section_banner($res[0]->post_title);
     echo $return_gallery;
+    var_dump($fields);
 ?>
 </section>
