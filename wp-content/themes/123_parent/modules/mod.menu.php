d29117d3c95bce04c581ebb5ebf652f0805821e2
Gus <gusta1296@gmail.com>
2019-04-18 15:31:02 -0700

    Finished Menu Six with tabs side menu and tabs pills

--- wp-content/themes/123_parent/modules/mod.menu.php
+++ wp-content/themes/123_parent/modules/mod.menu.php
@@ -24,171 +24,69 @@
 
 <?php
     $menu_tabs = 'menu_tabs_pills'; 
-    $menu_type = 'menu_four';
+    $menu_type = 'menu_six';
 ?>
     <!-- Menu Area -->
     <div id="menu_area" class="<?php echo ($menu_tabs == 'menu_tabs_side_menu') == true ? 'menu_flex' : ''; ?>">
         <!-- Menu -->
-        <?php if($menu_tabs == 'menu_tabs_pills'){ ?>
-        <div id="menu_tabs_pills">
+        <?php if($menu_tabs == 'menu_tabs_pills' || $menu_tabs == 'menu_tabs_side_menu'){ ?>
+        <div id="<?php echo $menu_tabs; ?>">
             <h3><a class="sides_tab active_tab grid-item" href="javascript:;">Sides</a></h3>
             <h3><a class="lunch_tab grid-item" href="javascript:;">Lunch</a></h3>
             <h3><a class="brunch_tab grid-item" href="javascript:;">Brunch</a></h3>
             <h3><a class="drinks_tab grid-item" href="javascript:;">Drinks</a></h3>
             <h3><a class="desserts_tab grid-item" href="javascript:;">Desserts</a></h3>
         </div>
-        <?php } else if($menu_tabs == 'menu_tabs_side_menu'){ ?>
-        <div id="menu_tabs_side_menu">
-            <h3><a class="sides_tab active_tab" href="javascript:;">Sides</a></h3>
-            <h3><a class="lunch_tab" href="javascript:;">Lunch</a></h3> 
-            <h3><a class="brunch_tab" href="javascript:;">Brunch</a></h3>
-            <h3><a class="drinks_tab" href="javascript:;">Drinks</a></h3>
-            <h3><a class="desserts_tab" href="javascript:;">Desserts</a></h3>
-        </div>
         <?php } ?>
-        <?php if($menu_type == 'menu_one'){ ?>
-        <!-- Menu List -->
-        <div id="menu_one">    
-            <h2 class="menu_title sides_title active_menu_title"><span>Sides</span></h2>
-        <?php }else if($menu_type == 'menu_two'){ ?>
-        <div id="menu_two">    
-            <h2 class="menu_title sides_title active_menu_title">Sides Menu</h2>
-            <h3 class="menu_subtitle sides_subtitle active_menu_subtitle">Sides Available until 5pm Saturday &#38; Sunday</h3>
-        <?php }else if($menu_type == 'menu_three'){ ?>
-        <div id="menu_three">
-            <h2 class="menu_title sides_title active_menu_title">Sides Menu</h2>
-            <h3 class="menu_subtitle sides_subtitle active_menu_subtitle">Sides Available until 5pm Saturday &#38; Sunday</h3>
-        <?php }else if($menu_type == 'menu_four'){ ?>
-        <div id="menu_four">
-            <h2 class="menu_title sides_title active_menu_title">Sides Menu</h2>
-            <h3 class="menu_subtitle sides_subtitle active_menu_subtitle">Sides Available until 5pm Saturday &#38; Sunday</h3>
-        <?php } 
-        if($menu_type == 'menu_one' || $menu_type == 'menu_two' || $menu_type == 'menu_three'){ ?>
-            <ul class="menu_section sides_section active_menu_section">
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-            </ul>
-        <?php }else if($menu_type == 'menu_four'){ ?>
-            <ul class="menu_section sides_section active_menu_section">
-                <li>
-                    <?php 
-                    $price = 6.75; 
-                    $price_array = explode('.',$price);
-                    ?>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-            </ul>
-        <?php } 
-        if($menu_type == 'menu_one'){ ?>
-            <h2 class="menu_title lunch_title"><span>Lunch</span></h2>
-        <?php }else if($menu_type == 'menu_two'){ ?>
-            <h2 class="menu_title lunch_title"><span>Lunch Menu</span></h2>
-            <h3 class="menu_subtitle lunch_subtitle">Lunch Available until 5pm Saturday &#38; Sunday</h3>
-        <?php }else if($menu_type == 'menu_three'){ ?>
-            <h2 class="menu_title lunch_title">Lunch Menu</h2>
-            <h3 class="menu_subtitle lunch_subtitle">Lunch Available until 5pm Saturday &#38; Sunday</h3>
-        <?php }else if($menu_type == 'menu_four'){ ?>
-            <h2 class="menu_title lunch_title">Lunch Menu</h2>
-            <h3 class="menu_subtitle lunch_subtitle">Lunch Available until 5pm Saturday &#38; Sunday</h3>
-        <?php } 
-        if($menu_type == 'menu_one' || $menu_type == 'menu_two' || $menu_type == 'menu_three'){ ?>
-            <ul class="menu_section lunch_section">
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <h3>Chips + Guacamole<span> $6.75</span></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-            </ul>
-        <?php }else if($menu_type == 'menu_four'){ ?>
-            <ul class="menu_section lunch_section">
-                <li>
-                    <?php 
-                    $price = 6.75; 
-                    $price_array = explode('.',$price);
-                    ?>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-                <li>
-                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
-                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
-                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
-                </li>
-            </ul>
+        <div id="<?php echo $menu_type; ?>"> 
+        <?php for($x = 0; $x < 2; $x++){ ?>
+        <!-- If Menu One -->
+        <?php if($menu_type == 'menu_one'){ ?>  
+            <h2 class="menu_title <?php echo ($x == 0) ? ' sides_title active_menu_title':'lunch_title';?>"><span><?php echo ($x == 0) ? 'Sides' : 'Lunch';?></span></h2>
+        <!-- If Menu Two, Three, Four or Six -->
+        <?php }else if($menu_type == 'menu_two' || $menu_type == 'menu_three' || $menu_type == 'menu_four' || $menu_type == 'menu_six'){ ?>   
+            <h2 class="menu_title <?php echo ($x == 0) ? ' sides_title active_menu_title':'lunch_title';?>"><?php echo ($x == 0) ? 'Sides' : 'Lunch';?> Menu</h2>
+            <h3 class="menu_subtitle <?php echo ($x == 0) ? ' sides_subtitle active_menu_subtitle':'lunch_subtitle';?>"><?php echo ($x == 0) ? 'Sides' : 'Lunch';?> Available until 5pm Saturday &#38; Sunday</h3>
         <?php } ?>
+        <!-- If Menu One, Two or Three -->
+        <?php if($menu_type == 'menu_one' || $menu_type == 'menu_two' || $menu_type == 'menu_three'){ ?>
+            <ul class="menu_section <?php echo ($x == 0) ? ' sides_section active_menu_section':'lunch_section'?>">
+            <?php for($i = 0; $i < 4; $i++){ ?>
+                <li>
+                    <h3>Chips + Guacamole<span> $6.75</span></h3>
+                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
+                </li>
+            <?php }?>
+            </ul>
+        <!-- If Menu Four -->
+        <?php }else if($menu_type == 'menu_four'){ ?>
+            <ul class="menu_section <?php echo ($x == 0) ? ' sides_section active_menu_section':'lunch_section'?>">
+            <?php for($i = 0; $i < 6; $i++){ 
+                    $price = 6.75; 
+                    $price_array = explode('.',$price); ?>
+                <li>
+                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
+                    <h3>Create a Sampler <div>$<?php echo $price_array[0] . '.<span>' . $price_array[1] . '</span>'; ?></div></h3>
+                    <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
+                </li>
+            <?php }?>
+            </ul>
+        <!-- If Menu Six -->
+        <?php }else if($menu_type == 'menu_six'){ ?>
+            <ul class="menu_section <?php echo ($x == 0) ? ' sides_section active_menu_section':'lunch_section'?>">
+            <?php for($i = 0; $i < 3; $i++){ ?>
+                <li>
+                    <div class="menu_image" style="background-image:url(http://123parent.localhost/wp-content/uploads/2019/04/WGC-Calabrian-Chili-Pasta-2-copy-2.jpg);"></div>
+                    <div>
+                        <h3>Coal Fired Blue Hubbard</h3>
+                        <p>Avocado, tomato, onion, cilantro, serrano chili, avocado, tomato, cilantro, serrano chili</p>
+                        <h4>$6.75</h4>
+                    </div>  
+                </li>
+            <?php }?>
+            </ul>            
+        <?php }
+        } ?>
         </div>
         <!-- /Menu List -->
     </div>
