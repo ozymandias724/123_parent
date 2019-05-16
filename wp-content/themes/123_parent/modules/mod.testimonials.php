37e8734f00ebb4943c17e6a0a20c63c55c333f8a
Gus <gusta1296@gmail.com>
2019-05-16 11:49:35 -0700

    Edited menus and styled testimonials

--- wp-content/themes/123_parent/modules/mod.testimonials.php
+++ wp-content/themes/123_parent/modules/mod.testimonials.php
@@ -22,7 +22,7 @@
     function _get_testimonials($fields)
     {
         $format = '
-            <div id="container">
+            <div class="container">
                 %s
                 %s
             </div>
@@ -39,7 +39,7 @@
     {
         $heading = (!empty($fields['featured_testimonials']['heading']) ? $fields['featured_testimonials']['heading'] : '');
         $details = (!empty($fields['featured_testimonials']['details']) ? $fields['featured_testimonials']['details'] : '');
-        $format = '<p>%s</p>%s';
+        $format = '<h2 class="testimonials_header">%s</h2>%s';
         $return = sprintf(
             $format
             ,$heading
@@ -49,7 +49,73 @@
     }
     function _get_body($fields)
     {
+        $return = '<ul>';
+        $format_text = '
+            <li class="testimonial_text">
+                %s
+                %s
+            </li>
+        ';
+        $format_image = '
+            <li class="testimonial_image">
+                %s
+                <div>
+                    %s
+                    %s
+                </div>
+            </li>
+        ';
+        $format_video = '
+            <li class="testimonial_video">
+                %s
+                <div>
+                    %s
+                    %s
+                </div>
+            </li>
+        ';
         
+        foreach($fields['featured_testimonials']['testimonials'] as $i => $item)
+        {
+            $testimonial = get_fields($item['testimonial']);
+            if($testimonial['status'] && $testimonial['type'] == 'text')
+            {
+                $return .= sprintf(
+                    $format_text
+                    ,(!empty($testimonial['details']) ? $testimonial['details'] : '')
+                    ,(!empty($testimonial['image']) ? '<h3>'.$item['testimonial']->post_title.'</h3>' : '')
+                );
+            }
+            else if($testimonial['status'] && $testimonial['type'] == 'image')
+            {
+                $return .= sprintf(
+                    $format_image
+                    ,(!empty($testimonial['image']) ? '<div class="block" style="background-image:url('.$testimonial['image']['url'].');"></div>' : '')
+                    ,(!empty($testimonial['details']) ? $testimonial['details'] : '')
+                    ,(!empty($testimonial['image']) ? '<h3>'.$item['testimonial']->post_title.'</h3>' : '')
+                );
+            }
+            else if($testimonial['status'] && $testimonial['type'] == 'video')
+            {
+                $video_format = '
+                    <video> 
+                        <source src="%s" type="video/mp4">
+                    </video>
+                ';
+                $video_tag = sprintf(
+                    $video_format
+                    ,(!empty($testimonial['video_url']) ? $testimonial['video_url'] : '')
+                );
+                $return .= sprintf(
+                    $format_video
+                    ,$video_tag
+                    ,(!empty($testimonial['details']) ? $testimonial['details'] : '')
+                    ,(!empty($testimonial['image']) ? '<h3>'.$item['testimonial']->post_title.'</h3>' : '')
+                );
+            }
+        }
+        $return .= '</ul>';
+        return $return;
     }
  ?>
 <section id="mod_testimonials">
