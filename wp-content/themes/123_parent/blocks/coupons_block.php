87ee3cc35a5a72e017d21a4648fc2fab33198fb4
kyle <kyle.marcy@gmail.com>
2019-06-10 19:11:33 -0400

    stopping point

--- wp-content/themes/123_parent/blocks/coupons_block.php
+++ wp-content/themes/123_parent/blocks/coupons_block.php
@@ -46,14 +46,17 @@
     ';
 
     $return['section'] .= sprintf(
-         $guide['section']
+        $guide['section']
+        // opts 
         ,( !empty($cB['anchor_enabled']) ? 'id="'.strtolower($cB['anchor_link_text']).'"' : '' ) // add an ID tag for the long scroll
         ,( !empty( $cB['width'] ) ? $cB['width'] : '' )                                                         // container width
         ,( !empty( $cB['background_color'] ) ? 'hasbg' :'' )                                                    // container has bg color class
         ,( !empty( $cB['background_color'] ) ? 'background-color:'.$cB['background_color'].';' : '' )           // container bg color style
         ,( !empty( $cB['foreground_color'] ) ? 'color:'.$cB['foreground_color'].';' : '' )           // container bg color style
-        ,( !empty($cB['heading']) ? '<h2>'.$cB['heading'].'</h2>' : '' )
-        ,( !empty($cB['text']) ? '<div>'.$cB['text'].'</div>' : '' )
+        // 
+        ,( !empty($cB['heading']) ? '<h2 class="site__fade site__fade-up block__heading">'.$cB['heading'].'</h2>' : '' )
+        ,( !empty($cB['text']) ? '<div class="site__fade site__fade-up block__details">'.$cB['text'].'</div>' : '' )
+        // 
         ,( !empty($return['coupon']) ? '<div class="site__grid">'.$return['coupon'].'</div>' : '' )
         ,( !empty($cB['view_all_button']['link']) ? '<a class="site__button" href="'.$cB['view_all_button']['link']['url'].'">'.$cB['view_all_button']['link']['title'].'</a>' : '' )
     );
