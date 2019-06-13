3b6d80510589a61dbc3dc5fe397d3e4d180cd9c6
kyle <kyle.marcy@gmail.com>
2019-06-12 23:52:28 -0400

    added the basics for the contact section; which will also be used as the 'areas served' section that rob mocked up. # this one will need some more fields FOR SURE, but for now the basics he wanted are there

--- wp-content/themes/123_parent/blocks/contact_block.php
+++ wp-content/themes/123_parent/blocks/contact_block.php
@@ -14,10 +14,76 @@
             <div class="container %s %s" style="%s %s">
                 %s
                 %s
+                <div class="columns">
+                    <div class="left">%s</div>
+                    <div class="right">%s</div>
+                </div>
             </div>
         </section>
     ';
 
+    $guide['locations'] = '<li>%s</li>';
+
+
+    
+    // 
+    // Do the left Side
+    $return['left'] = '';
+    foreach( $cB['left'] as $row ){
+
+        // Locations Layout
+        if( $row['acf_fc_layout'] == 'locations' ){
+            $return['left'] .= '<ul>';
+            foreach( $row['locations'] as $location ){
+                $fields = get_fields($location['location']->ID);
+                $return['left'] .= sprintf(
+                    $guide['locations']
+                    ,(!empty($fields['content']['heading']) ? $fields['content']['heading'] : '')
+                );
+            }
+            $return['left'] .= '</ul>';
+        }
+        
+        // Form Layout
+        if( $row['acf_fc_layout'] == 'form' ){
+            $return['left'] .= '<div>'.do_shortcode('[wpforms id="'.$row['form']->ID.'" title="false" description="false"]').'</div>';
+        }
+
+        // Map Layout
+
+    }
+    // 
+
+    // 
+    // Do the Right Side
+    $return['right'] = '';
+    foreach( $cB['right'] as $row ){
+
+        // Locations Layout
+        if( $row['acf_fc_layout'] == 'locations' ){
+            $return['right'] .= '<ul>';
+            foreach( $row['locations'] as $location ){
+                $fields = get_fields($location['location']->ID);
+                $return['right'] .= sprintf(
+                    $guide['locations']
+                    ,(!empty($fields['content']['heading']) ? $fields['content']['heading'] : '')
+                );
+            }
+            $return['right'] .= '</ul>';
+        }
+        
+        // Form Layout
+        if( $row['acf_fc_layout'] == 'form' ){
+            $return['right'] .= '<div>'.do_shortcode('[wpforms id="'.$row['form']->ID.'" title="false" description="false"]').'</div>';
+        }
+
+        // Map Layout
+
+    }
+    // 
+    
+
+    // write all the content we can into the opening until the left/right part
     $return['section'] .= sprintf(
         $guide['section']
         //  options for every block
@@ -29,6 +95,8 @@
         // post object grid options
         ,( !empty($cB['heading']) ? '<h2 class=" block__heading site__fade site__fade-up">'.$cB['heading'].'</h2>' : '' )
         ,( !empty($cB['text']) ? '<div class="block__details site__fade site__fade-up">'.$cB['text'].'</div>' : '' )
+        ,$return['left']
+        ,$return['right']
     );
 
 
