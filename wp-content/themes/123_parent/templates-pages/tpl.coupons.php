820a62c6b3c3982c7ebe15d7f9bc3298d6d85395
Gus <gusta1296@gmail.com>
2019-06-24 13:41:58 -0700

    finished coupons and services template page

--- wp-content/themes/123_parent/templates-pages/tpl.coupons.php
+++ wp-content/themes/123_parent/templates-pages/tpl.coupons.php
@@ -10,34 +10,59 @@ $args = array(
 $res = get_posts($args);
 $fields = get_fields(get_the_ID());
 
+print_r($res);
+
+$return['section'] = '';
+
 $guide['coupon'] = '
     <li class="site__fade site__fade-up">
         <a href="%s">
             <h5>%s</h5>
             <div class="coupon_description block__item-body">%s</div>
             %s
+            %s
         </a>
     </li>
 ';
 
 
-$return['coupon'] = '<section><ul>';
+$return['coupon'] = '<ul>';
 
 foreach($res as $i => $coupon) {
     
-    $fields = get_fields($coupon->ID);
+    $coupon_fields = get_fields($coupon->ID);
 
-    if( $fields['status'] ){ 
+    if( $coupon_fields['status'] ){ 
         $return['coupon'] .= sprintf(
             $guide['coupon']
             ,get_permalink($coupon->ID)
             ,$coupon->post_title
-            ,(!empty($fields['details']) ? $fields['details'] : '')
-            ,(!empty($fields['code']) ? '<p class="coupon_code">Code: <span>'.$fields['code'].'</span></p>' : '')
+            ,(!empty($coupon_fields['details']) ? $coupon_fields['details'] : '')
+            ,(!empty($coupon_fields['code']) ? '<p class="coupon_code">Code: <span>'.$coupon_fields['code'].'</span></p>' : '')
+            ,(!empty($coupon_fields['expiration']) ? '<p class="coupon_expiration">Expires: <span>'.$coupon_fields['expiration'].'</span></p>' : '')
         );
     }
 }
-$return['coupon'] .= '</ul></section>';
+$return['coupon'] .= '</ul>';
+
+$guide['section'] = '
+    <section class="page__template tpl_page_coupon">
+        <div class="container %s">
+            %s
+            %s
+            %s
+        </div>
+    </section>
+';
+
+$return['section'] .= sprintf(
+    $guide['section']
+    ,( !empty( $fields['width'] ) ? $fields['width'] : '' ) 
+    ,( !empty($fields['heading']) ? '<h2 class="site__fade site__fade-up block__heading" style="text-align:'.$fields['heading_alignment'].';">'.$fields['heading'].'</h2>' : '' )
+    ,( !empty($fields['text']) ? '<div class="site__fade site__fade-up block__details">'.$fields['text'].'</div>' : '' )
+    // 
+    ,( !empty($return['coupon']) ? '<div class="site__grid coupons__'.$fields['style'].'">'.$return['coupon'].'</div>' : '' )
+);
 
 ?>
 <?php
@@ -46,7 +71,7 @@ $return['coupon'] .= '</ul></section>';
 <main>
 <?php 
     include( get_template_directory() . '/parts/part.hero.php');
-    echo $return['coupon'];
+    echo $return['section'];
 ?>
 <?php 
     get_footer();
--- wp-content/themes/123_parent/templates-pages/tpl.services.php
+++ wp-content/themes/123_parent/templates-pages/tpl.services.php
@@ -2,26 +2,129 @@
 /**
 * Template Name: Services
 */
-    $fields_page = get_fields(get_the_ID());
+$args = array(
+    'post_type' => 'services',
+    'posts_per_page' => -1,
+);
 
-    $args = array(
-        'post_type' => 'services'
-        ,'posts_per_page' => -1
-    );
-    $services = get_posts($args);
 
-    foreach( $services as $service ){
+$res = get_posts($args);
+$fields = get_fields(get_the_ID());
 
-        $fields = get_fields($service->ID);
 
+$return = [];
+$return['section'] = '';
+$guide = [];
+$guide['services'] = '';
+
+$return['services'] = '<ul>';
+
+
+foreach($res as $i => $service){
+    $service_fields = get_fields($service->ID);
+    if( $fields['style'] == 'one' ){
+        $guide['services'] = '
+            <li>
+                <a href="%s">
+                    <div><div class="image '.( ($fields['style'] == 'one') ? 'less_size_block' : '').'" style="background-image:url(%s);"></div></div>
+                    <div>
+                        %s 
+                        <div class="service__details block__item-body">%s</div>
+                        <p class="service_price">%s</p>
+                    </div>
+                </a>
+            </li>
+        ';
+        if( $service_fields['status'] ){
+            $return['services'] .= sprintf(
+                $guide['services']
+                ,get_permalink($service->ID)
+                ,( !empty($service_fields['image']['url']) ? $service_fields['image']['url'] : '')
+                ,( !empty($service->post_title) ? '<h3>'.$service->post_title.'</h3>' : '')
+                ,( !empty($service_fields['details']) ? $service_fields['details'] : '')
+                ,( !empty($service_fields['price']) ? '$'.$service_fields['price'] : '')
+            );
+        }
+    }else if( $fields['style'] == 'two' || $fields['style'] == 'three' ){
+        if($i % 2 == 0){
+            $guide['services'] = '
+                <li>
+                    <a href="%s">
+                        <div><div class="image '.( ($fields['style'] == 'one') ? 'less_size_block' : '').'" style="background-image:url(%s);"></div></div>
+                        <div>
+                            %s 
+                            <div class="service__details block__item-body">%s</div>
+                        </div>
+                    </a>
+                </li>
+            ';
+            if( $service_fields['status'] ){ 
+                $return['services'] .= sprintf(
+                    $guide['services']
+                    ,get_permalink($service->ID)
+                    ,(!empty($service_fields['image']['url']) ? $service_fields['image']['url'] : '')
+                    ,( !empty($service->post_title) ? '<h3><span>'.$service->post_title.'</span><span>'.(!empty($service_fields['price']) ? '$'.$service_fields['price'] : '').'</span></h3>' : '' )
+                    ,(!empty($service_fields['details']) ? $service_fields['details'] : '')
+                );
+            }
+        }else{
+            $guide['services'] = '
+                <li>
+                    <a href="%s">
+                        <div>
+                            %s 
+                            <div class="service__details block__item-body">%s</div>
+                        </div>
+                        <div><div class="image '.( ($fields['style'] == 'one') ? 'less_size_block' : '').'" style="background-image:url(%s);"></div></div>
+                    </a>
+                </li>
+            ';
+            if( $service_fields['status'] ){ 
+                $return['services'] .= sprintf(
+                    $guide['services']
+                    ,get_permalink($service->ID)
+                    ,( !empty($service->post_title) ? '<h3><span>'.$service->post_title.'</span><span>'.(!empty($service_fields['price']) ? '$'.$service_fields['price'] : '').'</span></h3>' : '' )
+                    ,(!empty($service_fields['details']) ? $service_fields['details'] : '')
+                    ,(!empty($service_fields['image']['url']) ? $service_fields['image']['url'] : '')
+                );
+            }
+        }
     }
-    
+}
+
+$return['services'] .= '</ul>';
+
+// empty guide string 
+$guide['section'] = '
+    <section class="page__template tpl_page_service">
+        <div class="container %s services__%s">
+            %s
+            %s
+            %s
+        </div>
+    </section>
+';
+
+$return['section'] .= sprintf(
+    $guide['section']
+
+    ,( !empty( $fields['width'] ) ? $fields['width'] : '' )    // container width                                                    
+    ,( !empty( $fields['style']) ? $fields['style'] : '')                                                     
+    ,( !empty($fields['heading']) ? '<h2 class="block__heading" style="text-align:'.$fields['heading_alignment'].';">'.$fields['heading'].'</h2>' : '' )
+
+    ,( !empty($fields['text']) ? '<div class="block__details">'.$fields['text'].'</div>' : '' )
+
+    ,( !empty($return['services']) ? '<div '.( ($fields['style'] == 'one') ? 'class="site__grid"' : '').'>'.$return['services'].'</div>' : '' )
+);
+
+?>
+<?php
     get_header();
 ?>
-<main id="page_services">
+<main>
 <?php 
-    include( get_template_directory() . '/parts/part.hero.php'); 
-
+    include( get_template_directory() . '/parts/part.hero.php');
+    echo $return['section'];
 ?>
 <?php 
     get_footer();
