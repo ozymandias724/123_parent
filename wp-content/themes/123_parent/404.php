6b46fee66fdd58499e65e48b7f9d638aeb6d3550
Gus <gusta1296@gmail.com>
2019-07-02 15:09:51 -0700

    finished 404 page

--- wp-content/themes/123_parent/404.php
+++ wp-content/themes/123_parent/404.php
@@ -1,14 +1,28 @@
 <?php 
 /**
-* 
+*  404 Page
 */
+
+
+    $return = '
+        <section class="page_404">
+            <div class="container medium site__fade site__fade-up">
+                <h1>404</h1>
+                <h2>Sorry! Page Not Found!</h2>
+                <a class="page404_home site__button" href="'.get_site_url().'" title="Home link">Go to Home</a>
+            </div>
+        </section>
+    ';
+
     get_header();
 ?>
 <main class="page404">
 <?php 
 /**
- *      DO THE 404 PAGE PLEASE
+ *    DO THE 404 PAGE PLEASE
  */
- ?>
+    echo $return;
+?>
+
 </main>
 <?php get_footer(); ?>
