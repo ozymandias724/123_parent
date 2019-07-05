09927f57d749627df4e738a9b9fa37f06e0561b1
Gus <gusta1296@gmail.com>
2019-07-05 16:15:42 -0700

    finishing coupons content grid block styling

---
+++ wp-content/themes/123_parent/blocks/content_grid/parts/grid.service.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+* Service Grid
+*/
+
+    $return['content'] = '';
+
+    $guide['content'] = '
+        
+    ';
+
+?>
+<div class="grid_service">
+<?php
+    echo $return['content'];
+?>
+</div>
